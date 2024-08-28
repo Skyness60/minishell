6dd73542123d55a2fac352244ba3a891b4d0a26c
Jean Lebard <jlebard@made-f0Dr7s4.clusters.42paris.fr>
2024-08-28 18:14:17 +0200

    test

--- utils/prompt.c
+++ utils/prompt.c
@@ -6,32 +6,93 @@
 /*   By: jlebard <jlebard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 16:54:47 by jlebard           #+#    #+#             */
-/*   Updated: 2024/08/07 17:04:47 by jlebard          ###   ########.fr       */
+/*   Updated: 2024/08/28 16:50:25 by jlebard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-char	*get_var_env(char **env, char *var)
+#define PROMPT_SIZE 1024
+
+void	get_simpler_path(char *home_dir, char *path)
+{
+	size_t	home_dir_len;
+	size_t	path_len;
+
+	home_dir_len = 0;
+	path_len = 0;
+	if (ft_strncmp(home_dir, path, ft_strlen(home_dir)) == 0)
+	{
+		home_dir_len = ft_strlen(home_dir);
+		path_len = ft_strlen(path);
+	}
+	if (ft_strncmp(home_dir, path, path_len) == 0 || (ft_strncmp(home_dir, path, path_len - 1) == 0) &&
+		path[path_len] == '/')
+		ft_strlcpy(home_dir, "~", 1);
+	else
+	{
+		ft_memmove(path + 2, path + home_dir_len + 1, path_len - home_dir_len);
+		path[0] = '~';
+		path[1] = '/';
+		path[path_len - home_dir_len + 1] = '\0';		
+	}
+}
+
+char	*get_var_in_env(char **env, char *var)
 {
 	int	i;
-
+	
 	i = -1;
 	while (env[++i])
 	{
 		if (ft_strncmp(env[i], var, ft_strlen(var)) == 0 &&
-			env[ft_strlen(var) == '='])
-		return (env[i] + ft_strlen(var) + 1);
+			env[i][ft_strlen(var) == '='])
+		return (ft_strdup(env[i] + ft_strlen(var) + 1));
 	}
 	return (NULL);
 }
 
-char	*get_home(char **env)
+static void	free_strs(char *user, char *home_dir, char *cwd)
 {
-	
+	if (user)
+		free(user);
+	if (home_dir)
+		free(home_dir);
+	if (cwd)
+		free(cwd);
 }
 
 char	*create_prompt(char **env)
 {
-	
+	char	*home_dir;
+	char	*user;
+	char	*prompt;
+	char	*cwd;
+
+	user = get_var_in_env(env, "USER");
+	if (!user)
+		user = ft_strdup("user");
+	home_dir = get_var_in_env(env, "HOME");
+	printf("%s\n", home_dir);
+	cwd = getcwd(NULL, 0);
+	printf("%s\n", cwd);
+	if (!user || !home_dir || !cwd)
+		free_strs(user, home_dir, cwd);
+	get_simpler_path(home_dir, cwd);
+	prompt = malloc(PROMPT_SIZE);
+	prompt[0] = '\0';
+	printf("%s\n", home_dir);
+	printf("%s\n", cwd);
+	ft_strlcat(prompt, user, ft_strlen(user));
+	ft_strlcat(prompt + ft_strlen(prompt), "@minishell", ft_strlen("@minishell"));
+	ft_strlcat(prompt + ft_strlen(prompt), cwd, ft_strlen(cwd));
+	ft_strlcat(prompt + ft_strlen(prompt), "$ ", 2);
+	free(user);
+	return (prompt);
+}
+
+int main(int argc, char **argv, char **env)
+{
+	printf("%s", create_prompt(env));
+	return 1;
 }
