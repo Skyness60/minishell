db88e143305b37dd89cd447781c46d09bed0d777
Jean Lebard <jlebard@made-f0Br4s5.clusters.42paris.fr>
2024-10-07 14:22:31 +0200

    fix cmd

--- parsing/get_cmd.c
+++ parsing/get_cmd.c
@@ -6,7 +6,7 @@
 /*   By: jlebard <jlebard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 14:03:47 by jlebard           #+#    #+#             */
-/*   Updated: 2024/10/07 12:48:58 by jlebard          ###   ########.fr       */
+/*   Updated: 2024/10/07 14:21:38 by jlebard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,10 @@ static void	check_file_first(t_execs *exec, char **tab)
 				continue ;
 		}
 		else
+		{
 			exec->cmd = ft_strdup(tab[i]);
+			break ;
+		}
 	}	
 }	
 
