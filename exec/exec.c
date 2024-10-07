45c67791e1b9a89f9b86ec18cc9d3263954fba74
Sami Perron <sperron@made-f0Br5s6.clusters.42paris.fr>
2024-10-07 17:44:36 +0200

    save

--- exec/exec.c
+++ exec/exec.c
@@ -6,48 +6,51 @@
 /*   By: sperron <sperron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 16:55:28 by sperron           #+#    #+#             */
-/*   Updated: 2024/10/07 13:43:20 by sperron          ###   ########.fr       */
+/*   Updated: 2024/10/07 17:44:29 by sperron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+int	get_redirect_and_exec(int in_fd, int out_fd, t_execs *cmds)
+{
+	if (cmds->infile)
+	{
+		in_fd = open(cmds->infile, O_RDONLY);
+		if (in_fd == -1)
+			perror_exit("bash : No such file or directory", 1);
+		dup2(in_fd, STDIN_FILENO);
+		close(in_fd);
+		return (1);
+	}
+	if (cmds->outfile)
+	{
+		if (cmds->tronque == true)
+			out_fd = open(cmds->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		else
+			out_fd = open(cmds->outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
+		if (out_fd == -1)
+			perror_exit("bash : Permission Denied", 1);
+		dup2(out_fd, STDOUT_FILENO);
+		close(out_fd);
+		return (1);
+	}
+	return (0);
+}
 
 int execute_cmd(t_data *data, t_execs **cmds)
 {
     pid_t pid;
     int status;
 
-    status = is_builtin(data, 1, cmds[0]);
-    //if (status == 170)
-    //    return (printf("%s: %s: %s: numeric argument required\n", \
-	//	MS_NAME, cmds[0], cmds[1]), 150);
-    //else if (status == 150)
-    //    return (printf("%s: %s: %s: %s", MS_NAME, cmds[0], \
-	//	cmds[1], ERR_OPTIONS_HISTORY), 150);
-	//else if (status != 127)
-    //    return (status);
+	get_redirect_and_exec(data->in_fd, data->out_fd, cmds[0]);
+	status = is_builtin(data, data->out_fd, cmds[0]);
     pid = fork();
     if (pid == 0)
     {
-		if (cmds[0]->infile)
-		{
-			data->in_fd = open(cmds[0]->infile, O_RDONLY);
-			if (data->in_fd == -1)
-				perror_exit("bash : No such file or directory", 1);
-			dup2(data->in_fd, STDIN_FILENO);
-			close(data->in_fd);
-		}
-		if (cmds[0]->outfile)
-		{
-			if (cmds[0]->tronque == true)
-				data->out_fd = open(cmds[0]->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			else
-				data->out_fd = open(cmds[0]->outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
-			if (data->out_fd == -1)
-				perror_exit("bash : Permission Denied", 1);
-			dup2(data->out_fd, STDOUT_FILENO);
-			close(data->out_fd);
-		}
+		if (status != 127)
+			exit(status);
+		get_redirect_and_exec(data->in_fd, data->out_fd, cmds[0]);
         ft_execvp(data, cmds[0]);
         exit(1);
     }
